a8a9c3a5e0cf366fc9b741992657474fed0f0501
Hyukjin Kwon <hjnim0909@gmail.com>
2021-02-12 23:46:00 +0900

    Space Updated
    \n:\n

--- Day5/src/index.js
+++ Day5/src/index.js
@@ -8,29 +8,32 @@ function getTime() {
   // Set the date we're counting down to
   const xmasDay = new Date("2021-12-24:00:00:00+0900");
 
-  // Update the count down every 1 second
-  const x = setInterval(() => {
-    // Get today's date and time
-    const now = new Date().getTime();
+  // Get today's date and time
+  const now = new Date().getTime();
 
-    // Find the distance between now and the count down date
-    const distance = xmasDay - now;
+  // Find the distance between now and the count down date
+  const distance = xmasDay - now;
 
-    // Time calculations for days, hours, mins, and secs
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // Time calculations for days, hours, mins, and secs
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Display the result
-    clock.innerHTML = days + "d" + hours + "h" + minutes + "m" + seconds + "s";
-  }, 1000);
+  // Display the result
+  clock.innerHTML = `${days < 10 ? `0${days}d` : days + "D"}\n:\n
+  ${hours < 10 ? `0${hours}H` : hours + "H"}\n:\n
+  ${minutes < 10 ? `0${minutes} M` : minutes + "M"}\n:\n
+  ${seconds < 10 ? `0${seconds}S` : seconds + "S"}`;
 }
+
+//clock.innerHTML = days + "D" + hours + "H" + minutes + "M" + seconds + "S";
 
 function init() {
   getTime();
+  setInterval(getTime, 1000);
 }
 
 init();
